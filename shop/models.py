8c96e9b4c11f7fb65f9c8fb6db46dd8cc54b1481
njeruthuo <njeruthelearner@gmail.com>
2024-11-11 23:48:39 +0300

    coordinates add failed,adding users init

--- shop/models.py
+++ shop/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from stock.models import Product
 
 
+# from django.contrib.gis.db import models as gis_models
+
+
 class Shop(models.Model):
     branch_name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     opening_date = models.DateTimeField(auto_now_add=True)
     weight_tat = models.PositiveIntegerField(default=0)
     avg_weekly_profit = models.DecimalField(
-        decimal_places=2, max_digits=10, blank=True, null=True, default=0)
-    licenses = models.FileField(upload_to='licenses')
+        decimal_places=2, max_digits=10, blank=True, null=True, default=0
+    )
+    licenses = models.FileField(upload_to="licenses")
+    # coordinates = gis_models.PointField(
+    #     geography=True, blank=True, null=True, default="POINT(0 0)")
 
     def __str__(self):
         return f"Outlet distribution center {self.branch_name} - {self.location}"
