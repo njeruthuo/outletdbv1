0ef7f6df12db7bc6933294b1480309b9f7f28162
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-03-13 22:42:18 +0300

    path fix

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,11 @@
-# build_files.sh
-
 echo "Checking if Python is installed..."
 if ! command -v python3 &>/dev/null; then
     echo "Python3 not found, installing..."
     apt-get update && apt-get install -y python3 python3-pip
 fi
 
-export path = "usr/bin:$PATH"
+# Fixing PATH
+export PATH="/python312/bin:$PATH"
 
 echo "INSTALLING REQUIREMENTS....."
 
